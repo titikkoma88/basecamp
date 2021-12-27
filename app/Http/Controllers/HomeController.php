0da8a367b1687f851f6493234d4ff48b1a725b52
titikkoma88 <jie.koma88@gmail.com>
2021-12-27 22:28:00 +0700

    admin dashboard and fixing routing

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,18 +3,20 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use App\Models\Checkout;
 use Auth;
 
 class HomeController extends Controller
 {
     public function Dashboard()
     {
-        $checkouts = Checkout::with('Camp')->whereUserId(Auth::id())->get();
-        // $checkouts = Checkout::with('Camp')->where(user_id, Auth::id())->get();
-
-        return view('user.dashboard', [
-            'checkouts' => $checkouts
-        ]);
+        switch (Auth::user()->is_admin) {
+            case true:
+                return redirect(route('admin.dashboard'));
+                break;
+            
+            default:
+                return redirect(route('admin.dashboard'));
+                break;
+        }
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,8 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserController;
 use App\Http\Controllers\User\CheckoutController;
 use App\Http\Controllers\HomeController;
+use App\Http\Controllers\User\DashboardController as UserDashboard;
+use App\Http\Controllers\Admin\DashboardController as AdminDashboard;
 
 
 /*
@@ -33,9 +35,19 @@ Route::middleware(['auth'])->group(function () {
     Route::get('checkout/{camp:slug}', [CheckoutController::class, 'create'])->name('checkout.create');
     Route::post('checkout/{camp}', [CheckoutController::class, 'store'])->name('checkout.store');
     
-    // user dashboard
+    // dashboard
     Route::get('dashboard', [HomeController::class, 'dashboard'])->name('dashboard');
-    Route::get('dashboard/checkout/invoice/{checkout}', [CheckoutController::class, 'invoice'])->name('user.checkout.invoice');
+    
+    // user dashboard
+    Route::prefix('user/dashboard')->namespace('User')->name('user.')->group(function(){
+        Route::get('/', [UserDashboard::class, 'index'])->name('dashboard');
+    });
+
+    // admin dashboard
+    Route::prefix('admin/dashboard')->namespace('Admin')->name('admin.')->group(function(){
+        Route::get('/', [AdminDashboard::class, 'index'])->name('dashboard');
+    });
+
 });
 
 // Route::get('/dashboard', function () {
